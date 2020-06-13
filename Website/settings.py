927979d5ca488e02f40310d75b0e79ad3c3b4df8
mattdoyle04 <matt.doyle04@gmail.com>
2020-06-14 08:25:48 +1000

    allowed hosts

--- Website/settings.py
+++ Website/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'plktxbbtw@-79oo8+0lh&a#qm(bo*q
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
-ALLOWED_HOSTS = ['127.0.0.1','0.0.0.0','localhost']
+ALLOWED_HOSTS = ['127.0.0.1','0.0.0.0','localhost','puntingmix.herokuapp.com']
 
 
 # Application definition
